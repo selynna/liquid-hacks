8f87b602affabbe5760792a0d997370220c9addf
Darian Nguyen <17971850+nguyen-darianhuy@users.noreply.github.com>
2020-11-06 23:25:45 -0800

    Frontend: Fix build errors & install Valorant font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+const Header = styled.h1`
+  font-family: Valorant;
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Header>Valorant But Fantasy Football</Header>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
