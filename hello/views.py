59456f0f6d34bf79bf71489d17f83ce32e3a0190
Dan Zhang <dzhang50@hotmail.com>
2020-11-07 14:53:49 -0800

    Added rudimentary getplayer API function which accesses the Liquipedia API

--- hello/views.py
+++ hello/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+import os
 import requests
 
 # Create your views here.
@@ -10,6 +11,7 @@ import requests
 #    # return HttpResponse('Hello from Python!')
 #    return render(request, "index.html")
 def index(request):
+    print("INDEX")
     r = requests.get('http://httpbin.org/status/418')
     print(r.text)
     return HttpResponse('<pre>' + r.text + '</pre>')
@@ -24,5 +26,24 @@ def db(request):
     return render(request, "db.html", {"greetings": greetings})
 
 def getplayer(request):
-    player = "Dan"
-    return HttpResponse(player)
+    if request.method == 'GET':
+        print("Get player")
+        params = request.GET.dict()
+        print(params)
+        playerid = params["player"]
+        print("Extracted player id: ",id)
+        # Form Liquipedia API POST request
+        url = "https://api.liquipedia.net/api/v1/player"
+        data = {'apikey':os.environ.get('LIQUID_API_KEY'),
+                'wiki':'valorant',
+                'conditions':"[[id::"+playerid+"]]"
+                }
+        print("Sending request POST")
+        print("  URL: ",url)
+        print("  Params: ",data)
+        r = requests.post(url, data)
+        print("  Response: ",r.text)
+        return HttpResponse(r.text)
+    else:
+        print("Get player invalid method")
+        return HttpResponse("Invalid")
