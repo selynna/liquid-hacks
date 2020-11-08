a09017db6a4421b8ecf618ddccaee94c2e7dc5d2
Darian Nguyen <17971850+nguyen-darianhuy@users.noreply.github.com>
2020-11-07 22:04:23 -0800

    TeamSelect: Add hosted team logos

--- frontend/src/pages/pickems/TeamSelect.tsx
+++ frontend/src/pages/pickems/TeamSelect.tsx
@@ -9,6 +9,26 @@ import { P } from 'components/Text';
 
 import PlayerCell from './PlayerCell';
 
+const teamToLogo = {
+  'Cloud9 Blue': 'https://i.imgur.com/FQlShBU.png',
+  'Gen.G Esports': 'https://i.imgur.com/hzoTJyj.png',
+  'Team Envy': 'https://i.imgur.com/VL5iPJM.png',
+  TSM: 'https://i.imgur.com/zencKPh.png',
+  '100 Thieves': 'https://i.imgur.com/LI8nJ0I.png',
+  T1: 'https://i.imgur.com/xsnqlul.png',
+  Sentinels: 'https://i.imgur.com/HST365I.png',
+  'The Slimy Boogermen': 'https://i.imgur.com/tw9ixr1.png',
+  'Luminosity Gaming': 'https://i.imgur.com/BnjfZWu.png',
+  'Complexity Gaming': 'https://i.imgur.com/gP7R4E3.png',
+  Dignitas: 'https://i.imgur.com/EisGrug.png',
+  Renegades: 'https://i.imgur.com/UHw1VpC.png',
+  XSET: 'https://i.imgur.com/OWYBbXr.png',
+  'Equinox Esports': 'https://i.imgur.com/j2DIJjl.png',
+  'Spot Up': 'https://i.imgur.com/4qY7Lzu.png',
+  'Built By Gamers': 'https://i.imgur.com/mPAPorf.png',
+  'Team Liquid': 'https://i.imgur.com/OtZfAU8.png',
+};
+
 const selectStyles = {
   option: (provided: any, { isSelected, isFocused }: any) => {
     return {
@@ -63,7 +83,7 @@ const TeamSelect = ({ teams, curTeamName, setCurTeamName }) => {
               animate={{ x: 0, opacity: 1 }}
               exit={{ x: 150, opacity: 0 }}
               transition={{ duration: 0.3 }}
-              src={curTeam.logoUrl}
+              src={teamToLogo[curTeam.name]}
             />
           )}
         </AnimatePresence>
